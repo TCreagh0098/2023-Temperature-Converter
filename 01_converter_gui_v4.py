cf32ba928053c0ca6c0d998aef2f3e7a98421837
TCreagh0098 <125223853+TCreagh0098@users.noreply.github.com>
2024-02-16 09:37:33 +1300

    Add files via upload

--- 01_converter_gui_v4.py
+++ 01_converter_gui_v4.py
@@ -42,9 +42,9 @@ class Converter:
         self.temp_entry.grid(row=2, padx=10, pady=10)
 
         error = "Please enter a number"
-        self.temp_error = Label(self.temp_frame, text="",
-                                fg="#9C0000")
-        self.temp_error.grid(row=3)
+        self.output_label = Label(self.temp_frame, text="",
+                                  fg="#9C0000")
+        self.output_label.grid(row=3)
 
         # conversion, help and history / export buttons
         self.button_frame = Frame(self.temp_frame)
@@ -62,7 +62,8 @@ class Converter:
                                           text="To Farenheit",
                                           bg="#009900",
                                           fg=button_fg,
-                                          font=button_font, width=12)
+                                          font=button_font, width=12,
+                                          command=self.to_fahrenheit)
         self.to_farenheit_button.grid(row=0, column=1, padx=5, pady=5)
 
         self.to_help_info_button = Button(self.button_frame,
@@ -129,6 +130,15 @@ class Converter:
                                   "Celsius: ".format(to_convert))
         self.output_answer()
 
+    def to_fahrenheit(self):
+        to_convert = self.check_temp(-273)
+
+        if to_convert != "invalid":
+            # do calculation
+            self.var_feedback.set("Converting {} to "
+                                  "Fahrenheit: ".format(to_convert))
+        self.output_answer()
+
     # shows user output and clears entry widget
     # ready for next calculation
     def output_answer(self):
@@ -137,14 +147,14 @@ class Converter:
 
         if has_errors == "yes":
             # red text, pink entry box
-            self.temp_error.config(fg="#9C0000")
+            self.output_label.config(fg="#9C0000")
             self.temp_entry.config(bg="#F8CECC")
 
         else:
-            self.temp_error.config(fg="#004C00")
+            self.output_label.config(fg="#004C00")
             self.temp_entry.config(bg="#FFFFFF")
 
-        self.temp_error.config(text=output)
+        self.output_label.config(text=output)
 
 
 # main routine
